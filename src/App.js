76f3b2e164f1523fe1ee353300669f5d4f70f832
DilmurodjonT <dilmurodjontadjibaev@gmail.com>
2023-04-28 18:22:29 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/navbar/Navbar";
+import Auth from "./routes/auth/Auth";
+import Home from "./routes/home/Home";
+import Categories from "./routes/categories/Categories";
+import Product from "./routes/product/Product";
+import Register from "./routes/auth/register/Register";
+import Login from "./routes/auth/login/Login";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/auth" element={<Auth />}>
+          <Route path="/auth/register" element={<Register />} />
+          <Route path="/auth/login" element={<Login />} />
+        </Route>
+        <Route path="/categories" element={<Categories />} />
+        <Route path="/product" element={<Product />} />
+      </Routes>
+    </>
   );
 }
 
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import "./Navbar.css";
+import Container from "../../utils/Components";
+import qongiroq from "../../images/qo'giroq.svg";
+import savat from "../../images/savat.svg";
+import logo from "../../images/logo.png";
+import { Link } from "react-router-dom";
+import { useLocation } from "react-router-dom";
+
+const Navbar = () => {
+  const { pathname } = useLocation();
+  console.log(pathname);
+  return pathname.includes("/auth") ? (
+    <></>
+  ) : (
+    <div className="navbar">
+      <Container>
+        <div className="navber__wrapper">
+          <div className="navbar__left">
+            <span>Hi!</span>
+            <Link to="/auth/login" className="navbar__login">
+              Sign in
+            </Link>
+            <span>or</span>
+            <Link to="/auth/register" className="navbar__register">
+              register
+            </Link>
+          </div>
+          <div className="navbar__right">
+            <img src={qongiroq} alt="" />
+            <img src={savat} alt="" />
+          </div>
+        </div>
+        <div className="navbar__stick"></div>
+        <div className="logo__search">
+          <div className="logo">
+            <img src={logo} alt="" />
+          </div>
+        </div>
+      </Container>
+    </div>
+  );
+};
+
+export default Navbar;
